b1f0c70821cf56f2930701845998029448dabe75
Nathan <nable@ivytech.edu>
2023-04-19 09:04:42 -0400

    Build up through first classes activated in admin

---
+++ event_calendar/events/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    #Use path converter of int and str to create month and year url name within the path
+    path('', views.index, name="index"),
+    path('<int:year>/<str:month>/', views.index, name="index"),
+]
