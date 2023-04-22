93757223a9b41a29dd5ab5ea439696cd1e171c1c
Nathan <nable@ivytech.edu>
2023-04-22 09:18:46 -0400

    Created Account Registration

--- event_calendar/members/views.py
+++ event_calendar/members/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from .forms import RegisterUserForm
 
+#User login page
 def login_user(request, ):
     if request.method == "POST":
         username = request.POST["username"]
@@ -18,8 +21,24 @@ def login_user(request, ):
     else:
 
         return render(request, 'authenticate/login.html',{})
-    
+
+#Logout function (No individual page required)    
 def logout_user(request):
     logout(request)
     messages.success(request, 'Successfully Logged out')
     return redirect('index')
+
+def register_user(request):
+    if request.method == "POST":
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username ,password=password)
+            login(request, user)
+            messages.success(request, 'Account created successfully')
+            return redirect('index')
+    else:
+        form = RegisterUserForm()
+        return render(request, 'authenticate/register_user.html', {'form':form,})
